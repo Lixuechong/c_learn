177b59a5237987c767706a1427cb215bea563724
lichong <1379933500@qq.com>
2022-05-11 16:56:49 +0800

    # c++异常。

---
+++ c_learn/T25.cpp
@@ -0,0 +1,8 @@
+//
+//  T25.cpp
+//  c_learn
+//
+//  Created by lixuechong on 2022/5/11.
+//
+
+#include <stdio.h>
---
+++ c_learn/T26.cpp
@@ -0,0 +1,8 @@
+//
+//  T26.cpp
+//  c_learn
+//
+//  Created by lixuechong on 2022/5/11.
+//
+
+#include <stdio.h>
